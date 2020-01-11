6d8d26ad4e2fd320cd0caa729e5bb5fa36589718
Humam Shwaikh <hshwa039@uottawa.ca>
2020-01-10 19:55:29 -0500

    Done Logger

---
+++ Datalogger/Datalogger.ino
@@ -0,0 +1,74 @@
+#include <SPI.h>
+#include <SD.h>
+
+// Set the pins used
+#define cardSelect 4
+
+File logfile;
+
+// blink out an error code
+void error(uint8_t errno) {
+  while(1) {
+    uint8_t i;
+    for (i=0; i<errno; i++) {
+      digitalWrite(13, HIGH);
+      delay(100);
+      digitalWrite(13, LOW);
+      delay(100);
+    }
+    for (i=errno; i<10; i++) {
+      delay(200);
+    }
+  }
+}
+
+// This line is not needed if you have Adafruit SAMD board package 1.6.2+
+//   #define Serial SerialUSB
+
+void setup() {
+  pinMode(13, OUTPUT);
+
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(cardSelect)) {
+    error(2);
+  }
+  char filename[15];
+  strcpy(filename, "/ANALOG00.TXT");
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[7] = '0' + i/10;
+    filename[8] = '0' + i%10;
+    // create if does not exist, do not open existing, write, sync after write
+    if (! SD.exists(filename)) {
+      break;
+    }
+  }
+
+  logfile = SD.open(filename, FILE_WRITE);
+  if( ! logfile ) {
+    error(3);
+  }
+
+  pinMode(13, OUTPUT);
+  pinMode(8, OUTPUT);
+  
+  for (int i=0; i<10; i++) {
+    digitalWrite(8, HIGH);
+    delay(100);
+    digitalWrite(8, LOW);
+    delay(100);
+  }
+  
+}
+
+uint8_t i=0;
+void loop() {
+  logfile.print(analogRead(0));
+  logfile.print(",");
+  logfile.print(analogRead(1));
+  
+  //Write to SD Card
+  logfile.flush();
+  
+  delay(20);
+}
